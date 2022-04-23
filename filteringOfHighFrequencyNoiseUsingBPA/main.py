55d8ca76dbc0b5583463a33debd6056aa37c0d0f
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-04-23 21:22:41 +0300

    Update main.py

--- filteringOfHighFrequencyNoiseUsingBPA/main.py
+++ filteringOfHighFrequencyNoiseUsingBPA/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+
 data = [
 [2.0, 3.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
 [4.0, 1.0, 2.0, 2.0, 3.0, 0.0, 6.0, 0.0],
@@ -13,11 +14,13 @@ data = [
 [7.0, 4.0, 4.0, 5.0, 0.0, 5.0, 0.0, 4.0]
 ]
 
+
 data = np.array(data)
 length = data.shape[0]
 width = data.shape[1]
 x, y = np.meshgrid(np.arange(length), np.arange(width))
 fig = plt.figure()
+
 
 ax = fig.add_subplot(1, 1, 1, projection='3d')
 ax.plot_surface(x, y, data, cmap=cm.coolwarm)
